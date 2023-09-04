480706cf78d99014250cfe3b06ed1180756d0367
Jason Herald <jason.herald@bounteous.com>
2023-09-04 17:50:50 +0530

    Update usestate to object

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,30 +4,38 @@ import { addTask } from "../store/actions";
 import "./TaskForm.css";
 
 const TaskForm = () => {
-  const [taskName, setTaskName] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const [priority, setPriority] = useState(0);
+  const [state, setState] = useState({
+    taskName: "",
+    taskDescription: "",
+    dueDate: "",
+    priority: 0,
+  });
+
   const dispatch = useDispatch();
+  const updateState = (elementName, value) => {
+    setState({ ...state, [elementName]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (taskName.trim()) {
+    if (state.taskName.trim()) {
       dispatch(
         addTask({
-          name: taskName,
-          description: taskDescription,
+          name: state.taskName,
+          description: state.taskDescription,
           id: Date.now(),
-          dueDate: dueDate,
+          dueDate: state.dueDate,
           completed: false,
-          priority: priority,
+          priority: state.priority,
           createdDate: new Date().toISOString(),
         })
       );
-      setTaskName("");
-      setTaskDescription("");
-      setDueDate("");
-      setPriority(0);
+      setState({
+        taskName: "",
+        taskDescription: "",
+        dueDate: "",
+        priority: 0,
+      });
     }
   };
 
@@ -39,30 +47,33 @@ const TaskForm = () => {
           <label htmlFor="taskName">Task Name:</label>
           <input
             type="text"
+            name="taskName"
             id="taskName"
             className="task-input"
-            value={taskName}
-            onChange={(e) => setTaskName(e.target.value)}
+            value={state.taskName}
+            onChange={(e) => updateState(e.target.name, e.target.value)}
             required
           />
         </div>
         <div className="form-row">
           <label htmlFor="taskDescription">Task Description:</label>
           <textarea
+            name="taskDescription"
             id="taskDescription"
             className="task-input"
-            value={taskDescription}
-            onChange={(e) => setTaskDescription(e.target.value)}
+            value={state.taskDescription}
+            onChange={(e) => updateState(e.target.name, e.target.value)}
           />
         </div>
         <div className="form-row">
           <label htmlFor="dueDate">Due Date:</label>
           <input
             type="date"
+            name="dueDate"
             id="dueDate"
-            value={dueDate}
+            value={state.dueDate}
             min={new Date().toISOString().split("T")[0]}
-            onChange={(e) => setDueDate(e.target.value)}
+            onChange={(e) => updateState(e.target.name, e.target.value)}
             required
           />
         </div>
@@ -70,8 +81,9 @@ const TaskForm = () => {
           <label htmlFor="priority">Priority:</label>
           <select
             id="priority"
-            value={priority}
-            onChange={(e) => setPriority(Number(e.target.value))}
+            name="priority"
+            value={state.priority}
+            onChange={(e) => updateState(e.target.name, Number(e.target.value))}
             required
           >
             <option value={0}>High</option>
